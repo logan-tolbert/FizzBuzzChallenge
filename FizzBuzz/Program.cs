c04c1770e5a60b5e24c668e898d59d8efe58a32a
logan-tolbert <logo88205+git@gmail.com>
2024-08-26 08:46:08 -0500

    :tada: Initial Commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        FizzBuzz(2);
+        FizzBuzz(3);
+        FizzBuzz(5);
+        FizzBuzz(15);
+
+    }
+    public static void FizzBuzz(int num)
+    {
+        if (num % 15 == 0)
+        {
+            Console.WriteLine("Fizzbuzz");
+        }
+        else if (num % 5 == 0)
+        {
+            Console.WriteLine("Buzz");
+        }
+        else if (num % 3 == 0)
+        {
+            Console.WriteLine("Fizz");
+        }
+        else
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
+
